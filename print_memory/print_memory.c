ee7c9a618f89adf14738a815ad9db31126ea26b0
avardrobin <avardrobin@hotmail.com>
2016-04-08 22:03:57 +0200

    a tester en practice

--- print_memory/print_memory.c
+++ print_memory/print_memory.c
@@ -1,97 +1,80 @@
 #include <unistd.h>
 
-
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	decimal_to_hexa (char chiffre, int signe)
+void	ft_put_hexamemory(unsigned char c)
 {
-	if (signe == 1)
+	if (c < 16)
 	{
-		if (chiffre >= 0 && chiffre <= 9)
-			ft_putchar('0' + chiffre);
+		if (c <= 9)
+			ft_putchar('0' + c);
 		else
-			ft_putchar('a' + chiffre - 10);
+			ft_putchar('a' - 10 + c);
 	}
 	else
 	{
-		if (chiffre <= 0 && chiffre >= -5)
-			ft_putchar('f' + chiffre);
-		else
-			ft_putchar('9' + chiffre + 6);
+		ft_put_hexamemory(c / 16);
+		ft_put_hexamemory(c % 16);
 	}
 }
 
-void	ft_hexa_putchar(char c)
+void	ft_put_hexamemory_bis(unsigned char c)
 {
-	int	h;
-	int	u;
-
-	if (c >= 0)
-	{
-		h = c / 16;
-		u = c % 16;
-		decimal_to_hexa(h, 1);
-		decimal_to_hexa(u, 1);
-	}
-	else
-	{
-		h = (-1 -c) / 16;
-		u = (-1 -c) % 16;
-		decimal_to_hexa(-h, -1);
-		decimal_to_hexa(-u, -1);
-		
-	}
-}
-
-void	ft_put_carac(const void *addr, size_t size)
-{
-	size_t	i;
-
-	i = -1;
-	while (++i < size)
-	{
-		if (*((char *)addr + i) >= 33 && *((char *)addr + i) <= 126)
-			ft_putchar(*((char *)addr + i));
-		else
-			ft_putchar('.');
-	}
-	ft_putchar('\n');
-}
-
-void	spaces(int n)
-{
-	int	i;
-
-	i = -1;
-	while (++i < n)
-		ft_putchar(' ');
+	if (c < 16)
+		ft_putchar('0');
+	ft_put_hexamemory(c);
 }
 
 void	print_memory(const void *addr, size_t size)
 {
+	int	cpt;
+	int	l_cpt;
 	size_t	i;
-	int		cpt;
+	size_t	j;
+	int	first;
 
-	i = -1;
-	cpt = 0;
-	while (++i < size)
+	i = 0;
+	first = 0;
+	while (i < size)
 	{
-		ft_hexa_putchar(*((char *)addr + i));
-		cpt += 2;
-		if (i % 2 == 1)
+		cpt = 0;
+		j = i;
+		while (i < size && cpt < 40)
 		{
+			ft_put_hexamemory_bis(*((char *)addr + i));
+			i++;
+			cpt += 2;
+			if (i < size)
+			{
+				ft_put_hexamemory_bis(*((char *)addr + i));
+				cpt += 2;
+				i++;
+			}
 			ft_putchar(' ');
 			cpt++;
 		}
-		if ((i + 1) % 16 == 0)
-			ft_put_carac(addr + i - 15, 16);
-		else if (i == size - 1 && ((i + 1) % 16 != 0))
+		if (first == 0)
+			l_cpt = cpt;
+		else
 		{
-			spaces(40 - cpt % 40);
-			ft_put_carac(addr + i - ((i + 1) % 16), ((i + 1) % 16));
+			while (cpt != l_cpt)
+			{
+				ft_putchar(' ');
+				cpt++;
+			}
 		}
+		while (j < i)
+		{
+			if (*((char *)addr + j) >= 33 && *((char *)addr + j) <= 126) 
+				ft_putchar(*((char *)addr + j));
+			else
+				ft_putchar('.');
+			j++;
+		}
+		ft_putchar('\n');	
+		first = 1;
 	}
 }
